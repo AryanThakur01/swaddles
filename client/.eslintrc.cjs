0edaac9cc30b39462c5bdf21e675236e6dd40b9f
Aryan Thakur <aryan197297@gmail.com>
2023-05-23 00:00:51 +0530

    BEGIN THE SWADDLES PROJECT

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['@typescript-eslint', 'react-refresh', 'react-hooks'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
