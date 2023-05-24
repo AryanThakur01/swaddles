11733386b99a09176094ccfed9c15a83b67912ec
Aryan Thakur <aryan197297@gmail.com>
2023-05-24 17:08:42 +0530

    refactor: .eslintrc.cjs refactored

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -8,21 +8,14 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['@typescript-eslint', 'react-refresh', 'react-hooks'],
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
   overrides: [
     {
       files: ['src/**/*.ts', 'src/**/*.tsx'],
       extends: ['airbnb'],
-    },
-    {
-      files: ['*'],
-      excludedFiles: ['src/**/*.ts', 'src/**/*.tsx'],
       rules: {
-        // Disable all rules for files outside of 'src' directory
+        'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
       },
     },
   ],
