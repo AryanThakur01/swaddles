e6c35aaff502a8b4e30a0d8a74684f3294c1074b
Aryan Thakur <aryan197297@gmail.com>
2023-06-09 11:08:39 +0530

    update: the eslint configuration

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -3,46 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  files: ['src/**/*.ts'],
-  extends: [
-    'airbnb',
-    'standard-with-typescript',
-    'plugin: @typescript-eslint/recommended',
-    'plugin: import/errors',
-    'plugin: import/warnings',
-    'plugin: import/typescript',
-  ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  rules: {
-    'prettier/prettier': 'error',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'error',
-    'no-console': 'off',
-    'import/order': [
-      'error',
-      {
-        'newlines-between': 'never',
-        groups: [
-          ['builtin', 'external'],
-          ['internal', 'parent', 'sibling', 'index'],
-        ],
-      },
-    ],
-  },
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts'],
-    },
-    'import-resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: './tsconfig.json',
-      },
-    },
-  },
+  plugins: ["@typescript-eslint", "airbnb"],
+  rules: {},
 };
