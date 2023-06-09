956ea487332ce135dc0ab8aed009829df8f04f83
Aryan Thakur <aryan197297@gmail.com>
2023-06-09 17:17:07 +0530

    docs: Add technologies used and authentication end points

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # swaddles
+
 E-Commerce Website
+
+## Technologies Used
+
+#### Frontend
+
+- Typescript
+- ReactJs
+- Vite
+- Tailwind Css
+
+#### Backend
+
+- Node
+- Express
+- MongoDB
+
+#### Coding Style
+
+- Eslint "airbnb Guidelines"
+
+## Backend
+
+#### Authentication Routes
+
+- /api/v1/auth/ - get route - This is a test route for the authentication
+- /api/v1/auth/login - post route - login user
+- /api/v1/auth/register - post route - register user
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,22 @@
 import { config } from 'dotenv'
 import express from 'express'
+// ------------------ Calling the routes --------------------
 import notFound from './middleware/notFound'
 import authRouter from './routes/auth'
+// ----------------------------------------------------------
 
 const app = express()
+config()
 
 // --------------------- End Points -------------------------
 app.use('/api/v1/auth', authRouter)
 app.use(notFound)
-// -----------------------------------------------------------
+// ----------------------------------------------------------
 
-config()
+// ------------------Starting The Server---------------------
 const PORT: String | Number = process.env.PORT || 5001
 const start = () => {
   app.listen(PORT, () => console.log(`Backend running at port: ${PORT}`))
 }
 start()
+// ----------------------------------------------------------
